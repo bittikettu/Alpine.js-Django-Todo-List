e3023b43a6e2af19444176483891ae71d8df5f84
bittikettu <viidakkovekara@gmail.com>
2023-05-06 20:01:26 +0300

    Model relation work

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
+from django.contrib.auth.models import User, Group, Permission, AbstractUser
 # Register your models here.
 
 from .models import Task
-from .models import UserK
+from .models import Person
+from .models import TasksDone
+
+#class TasksInline(admin.StackedInline):
+#    model = TasksDone
+#    extra = 1
+
+# class PersonAdmin(UserAdmin):
+#    inlines = (EventsInline,)
+
+
+#class TaskAdmin(admin.ModelAdmin):
+#    inlines = (TasksInline,)
+
 
 admin.site.register(Task)
-
-admin.site.register(UserK, UserAdmin)
+admin.site.register(TasksDone)
+admin.site.register(Person, UserAdmin)
--- tasks/models.py
+++ tasks/models.py
@@ -1,14 +1,34 @@
 from django.db import models
 import datetime
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User, Group, Permission, AbstractUser
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
-    completed = models.BooleanField(default=False)
     deleted = models.BooleanField(default=False)
     price = models.BigIntegerField(default=0)
     added = models.DateTimeField(auto_now_add=True, blank=True)
-    finished = models.DateTimeField(auto_now_add=True, blank=True)
+    completed = models.BooleanField(default = False)
 
-class UserK(AbstractUser):
+    def __str__(self):
+        return f'({self.id}) {self.title} {self.price}€'
+
+class Person(AbstractUser):
+    tasks = models.ManyToManyField('Task', through = 'TasksDone', blank = True, default = None)
     pass
+
+class TasksDone(models.Model):
+    person = models.ForeignKey(Person, on_delete = models.CASCADE)
+    task = models.ForeignKey(Task, on_delete = models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+    done = models.BooleanField(default = False)
+    paid = models.BooleanField(default = False)
+
+    class Meta:
+        unique_together = [['person', 'task']]
+
+    def finishtask(self):
+        self.done = True
+        self.date_joined = datetime.date.today()
+
+    def __str__(self):
+        return f'{self.person.username} - {self.date} {self.task.title} {self.task.price}€'
