8c6459ebf9ef5a9a40919602b85adf7e9cb99245
bittikettu <viidakkovekara@gmail.com>
2023-05-06 16:11:44 +0300

    Added price of the task and some ui for the pricepoint

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+import datetime
 from django.contrib.auth.models import AbstractUser
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
     deleted = models.BooleanField(default=False)
+    price = models.BigIntegerField(default=0)
+    added = models.DateTimeField(auto_now_add=True, blank=True)
+    finished = models.DateTimeField(auto_now_add=True, blank=True)
 
 class UserK(AbstractUser):
     pass
--- tasks/views.py
+++ tasks/views.py
@@ -6,22 +6,37 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from tasks.models import Task
 from http import HTTPStatus
-
-
+from django.core.cache import cache
+import logging
 
 def task_list(request):
+    
+    logging.debug(cache.get('tasks'))
+    #print(cache.get('tasks'))
+    #kakke = cache.get('tasks')
+    #if(kakke == None):
     tasks = [{"id": task.id,
-              "title": task.title,
-              "completed": task.completed} for task in Task.objects.filter(deleted=False)]
+            "title": task.title,
+            "added": task.added,
+            "price": task.price,
+            "completed": task.completed} for task in Task.objects.filter(deleted=False)]
+    cache.set('tasks', tasks, 30)
+    #else:
+    #    print("kakkesta")
+    #    tasks = kakke
     return JsonResponse(status=HTTPStatus.OK, data=tasks, safe=False)
 
 def create_task(request):
     title = request.POST.get('title')
+    price = request.POST.get('price')
+    logging.debug(title)
     if not title:
         return JsonResponse(status=HTTPStatus.BAD_REQUEST, data={'error': 'title is required'})
-    task = Task.objects.create(title=title)
+    task = Task.objects.create(title=title, price=price)
     return JsonResponse(status=HTTPStatus.CREATED, data={'title': task.title,
+                                          'price': task.price,
                                           'completed': task.completed,
+                                          'added': task.added,
                                           'id': task.id}, safe=False)
 
 def delete_task(request, task_id):
