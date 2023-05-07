4a790b4e80570e442fd967ea8237a30a8a0707ca
Juha <viidakkovekara@gmail.com>
2023-05-07 22:35:11 +0300

    Sum calculation based on work done

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('user/balance', views.user_balance, name='user_balance'),
     path('tasks/', views.task_list, name='task_list'),
     path('tasks/create/', views.create_task, name='create_task'),
     path('tasks/<int:task_id>/delete/', views.delete_task, name='delete_task'),
--- tasks/views.py
+++ tasks/views.py
@@ -4,33 +4,47 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 from django.shortcuts import get_object_or_404
-from tasks.models import Task
+from tasks.models import Task, Person, TasksDone
 from http import HTTPStatus
 from django.core.cache import cache
 import logging
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.csrf import requires_csrf_token
+from django.db.models import Sum
 
-
+def user_balance(request):
+    userq = Person.objects.get(username='admin')
+    unpaid = Task.objects.filter(tasksdone__person=userq, tasksdone__paid=False, tasksdone__done=True).aggregate(Sum('price'))
+    paid = Task.objects.filter(tasksdone__person=userq, tasksdone__paid=True, tasksdone__done=True).aggregate(Sum('price'))
+    
+    balance = [{"paid":paid['price__sum']},{"unpaid":unpaid['price__sum']},{"user":userq.username}]
+    
+    print(balance)
+    return JsonResponse(status=HTTPStatus.OK, data=balance, safe=False)
 
 def task_list(request):
-    
+
     logging.debug(cache.get('tasks'))
-    #print(cache.get('tasks'))
-    #kakke = cache.get('tasks')
-    #if(kakke == None):
+    # print(cache.get('tasks'))
+    # kakke = cache.get('tasks')
+    # if(kakke == None):
+
     tasks = [{"id": task.id,
-            "title": task.title,
-            "added": task.added,
-            "price": task.price,
-            "completed": task.completed} for task in Task.objects.filter(deleted=False)]
-    cache.set('tasks', tasks, 30)
-    #else:
-    #    print("kakkesta")
-    #    tasks = kakke
+              "title": task.title,
+              "added": task.added,
+              "price": task.price,
+              "completed": task.completed} for task in Task.objects.filter(deleted=False).exclude(tasksdone__done=True)]
+
+    # cache.set('tasks', tasks, 30)
+
+    # test = Task.objects.filter(deleted=False)
+    # print(test)
+
     return JsonResponse(status=HTTPStatus.OK, data=tasks, safe=False)
 
-@csrf_exempt
+# @csrf_exempt
+
+
 def create_task(request):
     title = request.POST.get('title')
     price = request.POST.get('price')
@@ -39,30 +53,48 @@ def create_task(request):
         return JsonResponse(status=HTTPStatus.BAD_REQUEST, data={'error': 'title is required'})
     task = Task.objects.create(title=title, price=price)
     return JsonResponse(status=HTTPStatus.CREATED, data={'title': task.title,
-                                          'price': task.price,
-                                          'completed': task.completed,
-                                          'added': task.added,
-                                          'id': task.id}, safe=False)
+                                                         'price': task.price,
+                                                         'completed': task.completed,
+                                                         'added': task.added,
+                                                         'id': task.id}, safe=False)
 
-@csrf_exempt
+# @csrf_exempt
+
+
 def delete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id)
-    #task.delete()
+    # task.delete()
     task.deleted = True
     task.save()
     return JsonResponse(status=HTTPStatus.NO_CONTENT, data={'message': 'task deleted'})
 
-@csrf_exempt
+# @csrf_exempt
+
+
 def update_task_status(request, task_id):
     task = get_object_or_404(Task, pk=task_id)
     status = request.POST.get('status')
     user = request.POST.get('user')
-    print(user)
+    userq = Person.objects.get(username=user)
+
+    try:
+        obj = TasksDone.objects.get(person=userq, task=task)
+        obj.done = int(status)
+    except TasksDone.DoesNotExist:
+        obj = TasksDone(person=userq, task=task)
+        obj.done = int(status)
+
+    obj.save()
     if not status:
         return JsonResponse(status=HTTPStatus.BAD_REQUEST, data={'error': 'status is required'})
     task.completed = int(status)
     task.save()
+    # sum = TasksDone.objects.filter(person=userq,paid=False).aggregate(Sum('price'))
+    test = Task.objects.filter(
+        tasksdone__person=userq, tasksdone__paid=False, tasksdone__done=True).aggregate(Sum('price'))
+    print(test)
     return JsonResponse(status=HTTPStatus.NO_CONTENT, data={'message': 'task status updated'})
+
 
 def index(request):
     return render(request, 'index.html')
